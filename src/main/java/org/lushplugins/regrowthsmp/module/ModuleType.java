bd716160ad2d98fdcd467053a950135eb503470f
OakLoaf <oak@beaconstudios.org>
2024-09-04 13:15:06 +0100

    Fixed typo

--- src/main/java/org/lushplugins/regrowthsmp/module/ModuleType.java
+++ src/main/java/org/lushplugins/regrowthsmp/module/ModuleType.java
@@ -1,22 +1,24 @@
 package org.lushplugins.regrowthsmp.module;
 
 import org.lushplugins.lushlib.module.Module;
+import org.lushplugins.lushlib.plugin.SpigotPlugin;
+import org.lushplugins.regrowthsmp.RegrowthSMP;
 import org.lushplugins.regrowthsmp.module.crateanimation.CrateAnimation;
 
-import java.util.concurrent.Callable;
+import java.util.function.Function;
 
 public enum ModuleType {
     CRATE_ANIMATION(CrateAnimation::new);
 
-    private final Callable<Module> moduleCallable;
+    private final Function<SpigotPlugin, Module> moduleCallable;
 
-    ModuleType(Callable<Module> moduleCallable) {
+    ModuleType(Function<SpigotPlugin, Module> moduleCallable) {
         this.moduleCallable = moduleCallable;
     }
 
     public Module init() {
         try {
-            return moduleCallable.call();
+            return moduleCallable.apply(RegrowthSMP.getInstance());
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
