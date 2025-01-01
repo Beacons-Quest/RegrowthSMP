50f3c2fe3a2af5009ee37729ff43d9561b05df92
Oak <cooldcb@gmail.com>
2025-01-01 21:35:44 +0000

    Excluded spigot from nightcore

--- modules/crateanimation/build.gradle.kts
+++ modules/crateanimation/build.gradle.kts
@@ -2,5 +2,7 @@ dependencies {
     // Dependencies
     compileOnly(files("../../libraries/Animatronicsplugin.jar"))
     compileOnly(files("../../libraries/ExcellentCrates-5.3.2.jar"))
-    compileOnly("com.github.nulli0n:nightcore-spigot:v2.6.3-updated")
+    compileOnly("com.github.nulli0n:nightcore-spigot:v2.6.3-updated") {
+        exclude("org.spigotmc")
+    }
 }
